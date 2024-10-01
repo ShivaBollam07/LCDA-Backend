6def737ee0f41f62313cc2831e3fa9295003fb7f
Lekha Sathvik Devabathini <dlsathvik04@gmail.com>
2024-10-01 18:11:50 +0530

    added ViT implemetation to the dev branch

---
+++ README.md
@@ -0,0 +1,1 @@
+Add the model folder at models/plantvillage_final_model
--- app.py
+++ app.py
@@ -1,55 +1,58 @@
 import io
 import os
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
-from skimage.feature import graycoprops, graycomatrix
+
+from transformers import ViTForImageClassification, ViTImageProcessor
+import torch
 from PIL import Image
 
-import numpy as np
-import os
-import pickle
-
 load_dotenv(override=True)
-modelname = os.getenv("MODEL")
+modelName = os.getenv("MODEL")
 req_features = os.getenv("FEATURES")
 
-app = Flask(__name__)
 
-featuresmap = {
-    "c": "contrast",
-    "d": "dissimilarity",
-    "h": "homogeneity",
-    "e": "energy",
-    "C": "correlation",
-    "a": "ASM",
+app = Flask(__name__)
+modelPath = "./" + "models/" + modelName
+
+print("Loading the model---------")
+model = ViTForImageClassification.from_pretrained(modelPath)
+feature_extractor = ViTImageProcessor.from_pretrained(modelPath)
+model.eval()
+
+print("Model is Ready")
+
+id2label = {
+    "0": "Pepper bell Bacterial spot",
+    "1": "Pepper bell healthy",
+    "10": "Tomato Two spotted spider mite",
+    "11": "Tomato Target Spot",
+    "12": "Tomato Yellow Leaf Curl Virus",
+    "13": "Tomato mosaic virus",
+    "14": "Tomato healthy",
+    "2": "Potato Early blight",
+    "3": "Potato Late blight",
+    "4": "Potato healthy",
+    "5": "Tomato Bacterial spot",
+    "6": "Tomato Early blight",
+    "7": "Tomato Late blight",
+    "8": "Tomato Leaf Mold",
+    "9": "Tomato Septoria leaf spot",
 }
 
 
-properties = [featuresmap[i] for i in req_features]
-model = pickle.load(open("./models/" + modelname, "rb"))
 
-
-def get_image_feature_vec(image):
-    g = graycomatrix(np.array(image), [1, 2], [0, np.pi / 2], normed=True)
-    featurevec = []
-    for property in properties:
-        featurevec.append(graycoprops(g, property).flatten())
-
-    return np.concatenate(featurevec, axis=0, dtype=np.float32)
-
-def predict(image: np.ndarray):
-    features = get_image_feature_vec(image)
-    pred  =  model.predict([features])
-    if pred[0] == 1:
-        return "Leaf Curl Disease"
-    else:
-        return "Healthy"
-
-
+def predict(image):
+    inputs = feature_extractor(image, return_tensors="pt")
+    with torch.no_grad():
+        logits = model(**inputs).logits
+    predicted_label = logits.argmax(-1).item()
+    return id2label[str(predicted_label)]
 
 
 @app.route("/predict", methods=["POST"])
 def handle_predict():
+    print(request.files)
     if "image" not in request.files:
         return jsonify({"error": "No image part in the request"}), 400
 
@@ -60,15 +63,16 @@ def handle_predict():
 
     if file:
         try:
-            img = Image.open(io.BytesIO(file.read())).convert("L")
-            img_array = np.array(img)
-
-            return jsonify({"success": True, "prediction": predict(img_array)}), 200
+            img = Image.open(io.BytesIO(file.read()))
+            prediction = predict(img)
+            return jsonify({"success": True, "prediction": prediction}), 200
         except Exception as e:
+            print(e)
             return jsonify({"error": str(e)}), 500
+
+        
+
 
 
 if __name__ == "__main__":
     app.run(debug=False, port=os.getenv("PORT"))
-
-
